ae2c808020866fadee22826175ed4ce0c38ee6c8
iskewed <jtornello2019@hotmail.com>
2020-08-14 14:51:03 -0300

    Resolved a minimal bug in getInvertedDistanceToMiddle helper function | Added testing\components\common\HorizontalAccordion\Helper.spec.js and all the tests to the Helper functions

--- src/components/Common/HorizontalAccordion/Helper.js
+++ src/components/Common/HorizontalAccordion/Helper.js
@@ -7,36 +7,38 @@ export const getHalfIndexArray = (arr) => {
   return Math.floor(arr.length / 2) + additionalSubstract;
 };
 
-export const getDistanceToMiddle = (arr, n) => {
+export const getDistanceToMiddle = (arr, i) => {
   let distance = 0;
   const halfIndex = getHalfIndexArray(arr);
   if (halfIndex > 0) {
-    if (n < halfIndex) {
-      distance = Math.abs(n - halfIndex);
-    } else if (n > halfIndex) {
+    if (i < halfIndex) {
+      distance = Math.abs(i - halfIndex);
+    } else if (i > halfIndex) {
       let rigthHalf = halfIndex;
       if (isLengthEven(arr)) {
         rigthHalf += 1;
       }
-      distance = n - rigthHalf;
+      distance = i - rigthHalf;
     }
   }
 
   return distance;
 };
 
-export const getInvertedDistanceToMiddle = (arr, n) => {
+export const getInvertedDistanceToMiddle = (arr, i) => {
   const halfIndex = getHalfIndexArray(arr);
   let distance = halfIndex;
   if (halfIndex > 0) {
-    if (n < halfIndex) {
-      distance = halfIndex - Math.abs(n - halfIndex);
-    } else if (n > halfIndex) {
+    if (i < halfIndex) {
+      distance = halfIndex - Math.abs(i - halfIndex);
+    } else if (i > halfIndex) {
       let rightHalf = halfIndex;
       if (isLengthEven(arr)) {
         rightHalf += 1;
       }
-      distance = halfIndex - (n - halfIndex);
+      if (i > rightHalf) {
+        distance = arr.length - i - 1;
+      }
     }
   }
   return distance;
---
+++ src/testing/components/common/HorizontalAccordion/Helper.spec.js
@@ -0,0 +1,146 @@
+import * as Helper from "../../../../components/Common/HorizontalAccordion/Helper";
+
+describe("helperSlice", () => {
+  let evenArray;
+  let oddArray;
+  beforeEach(() => {
+    evenArray = [0, 1, 2, 3];
+    oddArray = [0, 1, 2, 3, 4];
+  });
+  describe("isLengthEven method", () => {
+    it("should return true if method has even length", () => {
+      const result = Helper.isLengthEven(evenArray);
+
+      expect(result).toBe(true);
+    });
+    it("should return false if method has odd length", () => {
+      oddArray = [1, 2, 3];
+
+      const result = Helper.isLengthEven(oddArray);
+
+      expect(result).toBe(false);
+    });
+  });
+  describe("getHalfIndexArray method", () => {
+    it("should return the index of the half number in the array if it's even", () => {
+      const result = Helper.getHalfIndexArray(evenArray);
+
+      expect(result).toBe(1);
+    });
+    it("should return the index of the half number in the array if it's odd", () => {
+      const result = Helper.getHalfIndexArray(oddArray);
+
+      expect(result).toBe(2);
+    });
+    it("should not throw an error and return 0 if the array is empty", () => {
+      let emptyArray = [0];
+
+      const result = Helper.getHalfIndexArray(emptyArray);
+
+      expect(result).toBe(0);
+    });
+  });
+  describe("getDistanceToMiddle method", () => {
+    it("should return the distance between this elements and the middle in the array if it's even", () => {
+      const distanceFromFirst = Helper.getDistanceToMiddle(evenArray, 0);
+      const distanceFromSecond = Helper.getDistanceToMiddle(evenArray, 1);
+      const distanceFromBeforeLast = Helper.getDistanceToMiddle(evenArray, 2);
+      const distanceFromLast = Helper.getDistanceToMiddle(evenArray, 3);
+
+      expect(distanceFromFirst).toBe(1);
+      expect(distanceFromSecond).toBe(0);
+      expect(distanceFromBeforeLast).toBe(0);
+      expect(distanceFromLast).toBe(1);
+    });
+    it("should return the distance between this elements and the middle in the EXTENDED array if it's even", () => {
+      const extendedArr = [...evenArray, 4, 5]; //0 1 2 3
+
+      const distanceFromFirst = Helper.getDistanceToMiddle(extendedArr, 0);
+      const distanceFromSecond = Helper.getDistanceToMiddle(extendedArr, 1);
+      const distanceFromThird = Helper.getDistanceToMiddle(extendedArr, 2);
+      const distanceFromFourth = Helper.getDistanceToMiddle(extendedArr, 3);
+      const distanceFromBeforeLast = Helper.getDistanceToMiddle(extendedArr, 4);
+      const distanceFromLast = Helper.getDistanceToMiddle(extendedArr, 5);
+
+      expect(distanceFromFirst).toBe(2);
+      expect(distanceFromSecond).toBe(1);
+      expect(distanceFromThird).toBe(0);
+      expect(distanceFromFourth).toBe(0);
+      expect(distanceFromBeforeLast).toBe(1);
+      expect(distanceFromLast).toBe(2);
+    });
+    it("should return the distance between this elements and the middle in the array if it's odd", () => {
+      const distanceFromFirst = Helper.getDistanceToMiddle(oddArray, 0);
+      const distanceFromSecond = Helper.getDistanceToMiddle(oddArray, 1);
+      const distanceFromThird = Helper.getDistanceToMiddle(oddArray, 2);
+      const distanceFromBeforeLast = Helper.getDistanceToMiddle(oddArray, 3);
+      const distanceFromLast = Helper.getDistanceToMiddle(oddArray, 4);
+
+      expect(distanceFromFirst).toBe(2);
+      expect(distanceFromSecond).toBe(1);
+      expect(distanceFromThird).toBe(0);
+      expect(distanceFromBeforeLast).toBe(1);
+      expect(distanceFromLast).toBe(2);
+    });
+    it("should return 0 each time if the array has only two elements", () => {
+      const arr = [0, 1];
+
+      const distanceFromFirst = Helper.getDistanceToMiddle(arr, 0);
+      const distanceFromSecond = Helper.getDistanceToMiddle(arr, 1);
+
+      expect(distanceFromFirst).toBe(0);
+      expect(distanceFromSecond).toBe(0);
+    });
+  });
+  describe("getInvertedDistanceToMiddle method", () => {
+    it("should return the inverted distance to middle between elements and middle if the array is even", () => {
+      let extendedArr = [...evenArray, 4, 5]; //0 1 2 3 4 5
+      const distanceFirst = Helper.getInvertedDistanceToMiddle(extendedArr, 0);
+      const distanceSecond = Helper.getInvertedDistanceToMiddle(extendedArr, 1);
+      const distanceThird = Helper.getInvertedDistanceToMiddle(extendedArr, 2);
+      const distanceFourth = Helper.getInvertedDistanceToMiddle(extendedArr, 3);
+      const distanceBefLast = Helper.getInvertedDistanceToMiddle(
+        extendedArr,
+        4
+      );
+      const distanceLast = Helper.getInvertedDistanceToMiddle(extendedArr, 5);
+
+      expect(distanceFirst).toBe(0);
+      expect(distanceSecond).toBe(1);
+      expect(distanceThird).toBe(2);
+      expect(distanceFourth).toBe(2);
+      expect(distanceBefLast).toBe(1);
+      expect(distanceLast).toBe(0);
+    });
+    it("should return the inverted distance to middle between elements and middle if the array is odd", () => {
+      let extendedArr = [...evenArray, 4, 5, 6]; //0 1 2 3 4 5 6
+      const distanceFirst = Helper.getInvertedDistanceToMiddle(extendedArr, 0);
+      const distanceSecond = Helper.getInvertedDistanceToMiddle(extendedArr, 1);
+      const distanceThird = Helper.getInvertedDistanceToMiddle(extendedArr, 2);
+      const distanceFourth = Helper.getInvertedDistanceToMiddle(extendedArr, 3);
+      const distanceFifth = Helper.getInvertedDistanceToMiddle(extendedArr, 4);
+      const distanceBefLast = Helper.getInvertedDistanceToMiddle(
+        extendedArr,
+        5
+      );
+      const distanceLast = Helper.getInvertedDistanceToMiddle(extendedArr, 6);
+
+      expect(distanceFirst).toBe(0);
+      expect(distanceSecond).toBe(1);
+      expect(distanceThird).toBe(2);
+      expect(distanceFourth).toBe(3);
+      expect(distanceFifth).toBe(2);
+      expect(distanceBefLast).toBe(1);
+      expect(distanceLast).toBe(0);
+    });
+    it("should return 0 each time if the array has only two elements", () => {
+      const arr = [0, 1];
+
+      const distanceFirst = Helper.getInvertedDistanceToMiddle(arr, 0);
+      const distanceSecond = Helper.getInvertedDistanceToMiddle(arr, 1);
+
+      expect(distanceFirst).toBe(0);
+      expect(distanceSecond).toBe(0);
+    });
+  });
+});
