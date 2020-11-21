45dc93cba4358e082782603847eabb8d09c708c0
iskewed <jtornello2019@hotmail.com>
2020-11-21 15:37:12 -0300

    Improvements in card visualization => -The bottom text is no longer overheaded

--- src/components/Pages/ListToSee/index.jsx
+++ src/components/Pages/ListToSee/index.jsx
@@ -1,21 +1,23 @@
-import React from "react";
-import { useSelector } from "react-redux";
-import { getAllMoviesInList } from "./../../../store/movies";
-import styles from "./styles";
-import HorizontalAccordion from "../../Common/HorizontalAccordion/";
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { getAllMoviesInList } from './../../../store/movies';
+import styles from './styles';
+import HorizontalAccordion from '../../Common/HorizontalAccordion/';
 
 const ListToSee = () => {
   const classes = styles();
   const listToSee = useSelector(getAllMoviesInList);
-  const types = ["movie", "series"];
+  const types = ['Movies', 'Series'];
   return (
     <React.Fragment>
       <div className={classes.accordionsContainer}>
         {types.map((t, i) => (
           <div key={`Accordion ${i}`} className={classes.accordionRow}>
-            <h1>{t}</h1>
+            <h1 style={{ margin: 10 }}>{t}</h1>
             <HorizontalAccordion
-              elements={listToSee.list.filter((e) => e.Type === t)}
+              elements={listToSee.list.filter(e =>
+                t.toUpperCase().includes(e.Type.toUpperCase())
+              )}
             />
           </div>
         ))}
