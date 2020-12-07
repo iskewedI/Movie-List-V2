2a53bb13f96a5766aa46c4b22f1fb69162a116df
iskewed <jtornello2019@hotmail.com>
2020-12-07 17:46:43 -0300

    Solved a bug where the List To See wasn't loading

--- src/components/Pages/ListToSee/index.jsx
+++ src/components/Pages/ListToSee/index.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-// import { getAllMoviesInList } from './../../../store/toSee';
+import { getAllMoviesInList } from './../../../store/movies';
 import styles from './styles';
 import HorizontalAccordion from '../../Common/HorizontalAccordion/';
 
 const ListToSee = () => {
   const classes = styles();
-  // const listToSee = useSelector(getAllMoviesInList);
-  const listToSee = { list: [] };
+  const listToSee = useSelector(getAllMoviesInList);
+  // const listToSee = { list: [] };
   const types = ['Movies', 'Series'];
-  
+
   return (
     <React.Fragment>
       <div className={classes.accordionsContainer}>
         {types.map((t, i) => (
           <div key={`Accordion ${i}`} className={classes.accordionRow}>
-            <h1 style={{ margin: 10, userSelect: "none" }}>{t}</h1>
+            <h1 style={{ margin: 10, userSelect: 'none' }}>{t}</h1>
             <HorizontalAccordion
               elements={listToSee.list.filter(e =>
                 t.toUpperCase().includes(e.Type.toUpperCase())
