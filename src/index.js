0b3154f80ef5d14cfab7ab8c382a2bdec7c930a8
iskewed <jtornello2019@hotmail.com>
2020-12-22 12:10:15 -0300

    Changed BrowserRouter to HashRouter to fix problems

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
-import "bootstrap/dist/js/bootstrap.min.js";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { HashRouter } from 'react-router-dom';
+import App from './App';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>,
-  document.getElementById("root")
+  </HashRouter>,
+  document.getElementById('root')
 );
