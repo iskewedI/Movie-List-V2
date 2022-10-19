8b690ae3d0c3439f1c8e914916dfb3fa675f3e9c
iskewedi <jtornello2019@hotmail.com>
2022-10-19 18:07:10 -0300

    - Now the user data get loaded when /me is loaded first time too

--- src/components/Pages/Me/index.jsx
+++ src/components/Pages/Me/index.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useTranslation } from 'react-i18next';
-import { getUserData } from '../../../store/user';
+import { getUser, getUserData, getUserLoaded, getUserLoading } from '../../../store/user';
+import { useEffect } from 'react';
 
 const Home = () => {
   const { t } = useTranslation();
 
+  const userLoaded = useSelector(getUserLoaded);
+  const userLoading = useSelector(getUserLoading);
+
   const userData = useSelector(getUserData);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (!userLoaded && !userLoading) {
+      dispatch(getUser());
+    }
+  });
 
   return (
     <div style={{ margin: '20px', color: 'white' }}>
